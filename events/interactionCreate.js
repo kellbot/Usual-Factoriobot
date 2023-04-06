3f459d8c3894e2bdcb77c0969ec7a311f5771d55
Kellbot <kellbot@gmail.com>
2023-04-06 11:07:10 +0000

    Created a bunch of new bugs

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -8,6 +8,7 @@ module.exports = {
 
 		const command = interaction.client.commands.get(interaction.commandName);
 
+		if (command === 'ignore')
 		if (!command)
 		{
 			console.error(`No command matching ${interaction.commandName} was found.`);
